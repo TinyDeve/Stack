bde8a43a41b692bf273ff335413f06555d918e8e
onur bakış <onur@onurs-MacBook-Pro.local>
2019-07-13 15:08:21 +0300

    Stack game basics are finished.

--- Assets/Scripts/Managers/GameManager.cs
+++ Assets/Scripts/Managers/GameManager.cs
@@ -1,19 +1,23 @@
 ﻿using UnityEngine;
 using DG.Tweening;
+using System;
 
 public class GameManager : MonoBehaviour
 {
     public static GameManager Instance;
 
     [SerializeField] private GameObject stackPrefab;
+    [SerializeField] private GameObject cutPrefab;
+    [SerializeField] private GameObject cubePrefab;
     [SerializeField] private Transform stacksParent;
+    [SerializeField] private Transform garbageParent;
 
     //Last stack info
     private Vector3 lastStackPosition;
     private Vector3 lastStackScale;
 
     //For stack cube pooling
-    private int stackCubePoolNumber = 50;
+    private int stackCubePoolNumber = 5;
     private StackCube[] stackCubes;
 
     //To keep track of active stack cube
@@ -28,29 +32,37 @@ public class GameManager : MonoBehaviour
     private int _stackNumber;
     private bool _leftRigth;
 
+    private int gameSpeed;
+
+    //To 
+    private bool gameFail;
+    private bool restarting;
+
     //To decrease gc
     private Vector3 objeDestination;
+    private Vector3 stackParentPosition;
 
     private void Awake()
     {
         Instance = this;
+        gameSpeed = 2;
+
+
         lastStackPosition = new Vector3(0, 0, 0);
+        lastStackScale = new Vector3(5, 1, 5);
         objeDestination = new Vector3(0, 0, 0);
         _stackNumber = 0;
         _leftRigth = true;
-
-        //Stacks
-        stacksParent.DOMove(new Vector3(0, -1000, 0), 6000);
-
         //Stack pooling
         CreatePool();
+
     }
 
     private void Start()
     {
         activeStackCubeIndex = 0;
         activeStackCube = stackCubes[0];
-        InvokeRepeating("SendStack",0,4);
+        SendStack();
     }
 
 
@@ -60,7 +72,7 @@ public class GameManager : MonoBehaviour
     }
 
 
-    private void SendStack()
+    public void SendStack()
     {
         //Change direction and add a stack
         _leftRigth = !_leftRigth;
@@ -74,7 +86,7 @@ public class GameManager : MonoBehaviour
 
         objeDestination.y = _stackNumber;
 
-        activeStackCube.SetPosition(objeDestination);
+        activeStackCube.SetPositionScale(objeDestination,lastStackScale);
 
         //Activate last cube
         activeStackCube.Active(true);
@@ -84,20 +96,20 @@ public class GameManager : MonoBehaviour
 
         objeDestination.y = _stackNumber;
 
-        activeStackCube.Move(objeDestination, 2);
+        activeStackCube.Move(objeDestination, gameSpeed);
     }
 
     private void GetInput()
     {
         if (Input.GetMouseButtonDown(0))
         {
+            if (gameFail && !restarting)
+            {
+                Restart();
+                return;
+            }
             StopLastStack();
         }
-    }
-
-    private void StopLastStack()
-    {
-        stackCubes[activeStackCubeIndex].Stop(lastStackPosition, lastStackScale);
     }
 
     public void SetLastStackInfo(Vector3 position, Vector3 scale) 
@@ -106,6 +118,87 @@ public class GameManager : MonoBehaviour
 
         lastStackPosition = position;
     }
+
+    public void FailGame() 
+    {
+        Debug.Log("GameFail");
+        gameFail = true;
+    }
+
+    public void Restart() 
+    {
+        restarting = true;
+        stacksParent.DOMove(Vector3.zero, gameSpeed).OnComplete(EmptyAllObjects);
+    }
+
+    public void EmptyAllObjects() 
+    {
+        lastStackPosition = new Vector3(0, 0, 0);
+        lastStackScale = new Vector3(5, 1, 5);
+        objeDestination = new Vector3(0, 0, 0);
+        _stackNumber = 0;
+        _leftRigth = true;
+
+
+        CleanObjects();
+
+
+        activeStackCubeIndex = 0;
+        activeStackCube = stackCubes[0];
+        SendStack();
+
+        restarting = false;
+        gameFail = false;
+    }
+
+    private void CleanObjects()
+    {
+        for (int i = 0; i < garbageParent.transform.childCount; i++) 
+        {
+            Destroy(garbageParent.GetChild(i).gameObject);
+        }
+    }
+
+    public void MoveBelow() 
+    {
+        stackParentPosition = stacksParent.transform.position;
+
+        stackParentPosition -= stackPieceHeigth;
+
+        stacksParent.DOMove(stackParentPosition,gameSpeed);
+    }
+
+    #region Cutting
+
+    private void StopLastStack()
+    {
+        stackCubes[activeStackCubeIndex].Stop(lastStackPosition, lastStackScale);
+        MoveBelow();
+    }
+
+    public void PlaceCutObje(Vector3 position,Vector3 scale) 
+    {
+        GameObject a = Instantiate(cutPrefab, garbageParent) as GameObject;
+
+        a.transform.localPosition = position;
+        a.transform.localScale = scale;
+
+    }
+
+    public void PlaceObje(Vector3 position, Vector3 scale)
+    {
+        GameObject a = Instantiate(cubePrefab, garbageParent) as GameObject;
+
+        a.transform.localPosition = position;
+        a.transform.localScale = scale;
+    }
+
+    private void GameLosse() 
+    { 
+
+    }
+
+    #endregion
 
     #region StackCube Pooling
     public void CreatePool()
--- Assets/Scripts/Objects/StackCube.cs
+++ Assets/Scripts/Objects/StackCube.cs
@@ -5,10 +5,17 @@ public class StackCube : MonoBehaviour
 {
     [SerializeField] private Transform _transform;
     [SerializeField] private MeshRenderer _meshRenderer;
+    [SerializeField] private BoxCollider _boxCollider;
 
     private GameManager _gameManager;
     public int cubeId;
-    
+
+    private Vector3 stackLocalPosition;
+    private Vector3 stackLocalScale;
+
+    private Vector3 ObjePosition;
+
+    private bool isActive;
 
     private void Awake()
     {
@@ -25,29 +32,170 @@ public class StackCube : MonoBehaviour
     public void Active(bool active)
     {
         _meshRenderer.enabled = active;
+        _boxCollider.enabled = active;
+        isActive = active;
     }
 
     public void Stop(Vector3 lastStackPosition,Vector3 lastStackScale) 
     {
+        if (!isActive)
+        {
+            return;
+        }
         _transform.DOKill();
         CutStackObje(lastStackPosition,lastStackScale);
 
         //Set some data for next move stop and cut operations
-        _gameManager.SetLastStackInfo(_transform.localPosition, _transform.localScale);
     }
 
     private void CutStackObje(Vector3 lastStackPosition, Vector3 lastStackScale)
     {
+        Active(false);
+
         //Cut this stack obje create one piece trash obje
+
+        stackLocalPosition = _transform.localPosition;
+        stackLocalScale = _transform.localScale;
+
+        float zEnd = stackLocalPosition.z - lastStackPosition.z;
+        float xEnd = stackLocalPosition.x - lastStackPosition.x;
+        Vector3 objeScale = Vector3.zero;
+
+
+        //Fail Game
+
+        if (!(zEnd < stackLocalScale.z && zEnd > -stackLocalScale.z))
+        {
+            _gameManager.FailGame();
+            _gameManager.PlaceCutObje(stackLocalPosition, stackLocalScale);
+            return;
+        }
+        if (!(xEnd < stackLocalScale.x && xEnd > -stackLocalScale.x))
+        {
+            _gameManager.FailGame();
+            _gameManager.PlaceCutObje(stackLocalPosition, stackLocalScale);
+            return;
+        }
+
+
+        if (!(zEnd < 0.1 && zEnd > -0.1))
+        {
+            if (zEnd > 0) 
+            {
+                //Calculate position and scale to place obje
+                ObjePosition = stackLocalPosition;
+                ObjePosition.z += stackLocalScale.z / 2 - zEnd / 2;
+
+                objeScale = stackLocalScale;
+                objeScale.z = zEnd;
+
+                _gameManager.PlaceCutObje(ObjePosition, objeScale);
+
+                //Calculate position and scale to place obje
+                ObjePosition = stackLocalPosition;
+                ObjePosition.z += -zEnd / 2;
+                objeScale = stackLocalScale;
+                objeScale.z -= zEnd;
+            }else
+            {
+
+                //Calculate position and scale to place obje
+                ObjePosition = stackLocalPosition;
+                ObjePosition.z -= (stackLocalScale.z / 2 + zEnd / 2); //xEnd Negatif
+
+                objeScale = stackLocalScale;
+                objeScale.z = -zEnd;//xEnd Negatif
+
+                _gameManager.PlaceCutObje(ObjePosition, objeScale);
+
+
+                //Calculate position and scale to place obje
+                ObjePosition = stackLocalPosition;
+                ObjePosition.z -= zEnd / 2;
+
+                objeScale = stackLocalScale;
+                objeScale.z += zEnd;
+            }
+        }
+        else if(!(xEnd < 0.1 && xEnd > -0.1))
+        {
+            if (xEnd > 0)
+            {
+                Debug.Log("rigth");
+                //Calculate position and scale to place obje
+                ObjePosition = stackLocalPosition;
+                ObjePosition.x += stackLocalScale.x / 2 - xEnd / 2;
+
+                objeScale = stackLocalScale;
+                objeScale.x = xEnd;
+
+                _gameManager.PlaceCutObje(ObjePosition, objeScale);
+
+
+                //Calculate position and scale to place obje
+                ObjePosition = stackLocalPosition;
+                ObjePosition.x += -xEnd / 2;
+
+                objeScale = stackLocalScale;
+                objeScale.x -= xEnd;
+
+            }
+            else
+            {
+                //Calculate position and scale to place obje
+                ObjePosition = stackLocalPosition;
+                ObjePosition.x -= (stackLocalScale.x / 2 + xEnd / 2); //xEnd Negatif
+
+                objeScale = stackLocalScale;
+                objeScale.x = -xEnd;//xEnd Negatif
+
+                _gameManager.PlaceCutObje(ObjePosition, objeScale);
+
+
+                //Calculate position and scale to place obje
+                ObjePosition = stackLocalPosition;
+                ObjePosition.x -= xEnd / 2;
+
+                objeScale = stackLocalScale;
+                objeScale.x += xEnd;
+            }
+        }
+        else 
+        {
+            //Score
+            ObjePosition = stackLocalPosition;
+            objeScale = stackLocalScale;
+        }
+
+        _gameManager.PlaceObje(ObjePosition, objeScale);
+
+        _gameManager.SetLastStackInfo(ObjePosition, objeScale);
+
+        _gameManager.SendStack();
+
     }
 
-    public void SetPosition(Vector3 destination) 
+    public void CheckFailed() 
+    {
+         
+    }
+
+    public void SetPositionScale(Vector3 destination,Vector3 scale) 
     {
         _transform.localPosition = destination;
+        _transform.localScale = scale;
+
     }
 
     public void Move(Vector3 destination,int gameSpeed)
     {
-        _transform.DOLocalMove(destination,gameSpeed);
+        Vector3 direction = destination - _transform.localPosition;
+
+
+
+        _transform
+            .DOLocalMove(destination + direction, gameSpeed)
+            .SetEase(Ease.Linear)
+            .SetLoops(-1, LoopType.Yoyo);
     }
 }
